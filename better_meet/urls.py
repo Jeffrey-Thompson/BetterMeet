4ae5b5f2cc9b1133c48dc321f2b99b276fc94b65
JeffreyThompson <jeffathomp@gmail.com>
2020-10-27 11:40:15 -0500

    login/logout

--- better_meet/urls.py
+++ better_meet/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     # Sends to main_app
     path('', include('main_app.urls')),
+    path('accounts/', include('django.contrib.auth.urls'))
 ]
