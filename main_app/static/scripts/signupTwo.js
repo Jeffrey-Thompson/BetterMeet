314b4b3bcf6524c24410fb7c16a6be2adcbd2361
JeffreyThompson <jeffathomp@gmail.com>
2020-10-28 17:59:50 -0500

    fix remove interests bug

--- main_app/static/scripts/signupTwo.js
+++ main_app/static/scripts/signupTwo.js
@@ -1,7 +1,14 @@
 let interests = []
 
 const changeInterests = function(id) {
-    interests.push(id.value)
+    const interest = id.value
+    const present = interests.includes(interest);
+    if (present) {
+        const index = interests.indexOf(interest);
+        interests.splice(index, 1);
+    } else {
+        interests.push(id.value);
+    }
     console.log(interests);
     document.getElementById('interests_input').value = interests.join()
 }
