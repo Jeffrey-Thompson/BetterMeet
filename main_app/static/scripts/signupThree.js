6297ac2ce6be028dd27258da251ef445437e04df
JeffreyThompson <jeffathomp@gmail.com>
2020-10-29 11:18:13 -0500

    javascript for signup three

--- main_app/static/scripts/signupThree.js
+++ main_app/static/scripts/signupThree.js
@@ -1,6 +1,9 @@
 let genders = [];
 let bodyType = [];
 let relationshipStatus = [];
+let education = [];
+let religion = [];
+let race = [];
 
 const changeGenders = function(id) {
     const gender = id.value;
@@ -38,5 +41,44 @@ const changeRelationship = function(id) {
         relationshipStatus.push(id.value);
     }
     console.log(relationshipStatus);
-    document.getElementById('body_type_input').value = bodyType.join();
+    document.getElementById('relationship_status_input').value = relationshipStatus.join();
+}
+
+const changeEducation = function(id) {
+    const level = id.value;
+    const present = education.includes(level);
+    if (present) {
+        const index = education.indexOf(level);
+        education.splice(index, 1);
+    } else {
+        education.push(id.value);
+    }
+    console.log(education);
+    document.getElementById('education_input').value = education.join();
+}
+
+const changeReligion = function(id) {
+    const kind = id.value;
+    const present = religion.includes(kind);
+    if (present) {
+        const index = religion.indexOf(kind);
+        religion.splice(index, 1);
+    } else {
+        religion.push(id.value);
+    }
+    console.log(religion);
+    document.getElementById('religion_input').value = religion.join();
+}
+
+const changeRace = function(id) {
+    const ethnicity = id.value;
+    const present = race.includes(ethnicity);
+    if (present) {
+        const index = race.indexOf(ethnicity);
+        race.splice(index, 1);
+    } else {
+        race.push(id.value);
+    }
+    console.log(race);
+    document.getElementById('race_input').value = race.join();
 }
