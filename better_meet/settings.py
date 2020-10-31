79ccff22a7e8eb834e2b127b899291250fe7b16c
JeffreyThompson <jeffathomp@gmail.com>
2020-10-31 10:47:35 -0500

    crispy added to django forms

--- better_meet/settings.py
+++ better_meet/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main_app', 
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,4 @@ STATIC_URL = '/static/'
 LOGIN_REDIRECT_URL = '/profiles/index'
 LOGOUT_REDIRECT_URL = '/'
 
+CRISPY_TEMPLATE_PACK = 'uni_form'
